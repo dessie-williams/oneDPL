bab2defdc4b2dcd7df12395e1ab5732d5f26e9af
Dessie Williams <dessie.williams@intel.com>
2023-04-14 13:01:11 -0400

    Update win_matrix_test.bat

--- .github/scripts/win_matrix_test.bat
+++ .github/scripts/win_matrix_test.bat
@@ -23,8 +23,7 @@ if "%TMP_INTEL_LLVM_COMPILER%" == "TRUE" (
   powershell $output = %matrix.cxx_compiler% --version; Write-Host ::warning::Compiler: $output
   powershell -Command "(Get-Content '%CONDA_PREFIX%\Library\lib\cl.cfg') -replace 'CL_CONFIG_TBB_DLL_PATH = .*', 'CL_CONFIG_TBB_DLL_PATH = %CONDA_PREFIX%\Library\bin' | Out-File -encoding ASCII -FilePath '%CONDA_PREFIX%\Library\lib\cl.cfg'"
 )        
-# ninja program arguments
-$cmd_args = @"
+
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 set INCLUDE=%CONDA_PREFIX%\Library\include;%INCLUDE%
 set LIB=%CONDA_PREFIX%\Library\lib;%LIB%
@@ -36,10 +35,5 @@ mkdir build && cd build
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=%matrix.build_type% -DCMAKE_CXX_STANDARD=%matrix.std% -DCMAKE_CXX_COMPILER=%matrix.cxx_compiler% -DONEDPL_BACKEND=%matrix.backend% -DONEDPL_DEVICE_TYPE=%matrix.device_type% ..
 ninja -j 2 -v %ninja_targets%
 ctest --timeout %TEST_TIMEOUT% -C %matrix.build_type% --output-on-failure %ctest_flags%
-"@
-
-
-$cmd_args = [string]::join(" ", ($cmd_args.Split("`n")))
-cmd /v /c $cmd_args
 
 exit /b 0
