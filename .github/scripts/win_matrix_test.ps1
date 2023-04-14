e642aadc47425c55761b4f64228f1eeabb77ea4e
Dessie Williams <dessie.williams@intel.com>
2023-04-14 12:23:33 -0400

    Update win_matrix_test.ps1

--- .github/scripts/win_matrix_test.ps1
+++ .github/scripts/win_matrix_test.ps1
@@ -27,9 +27,9 @@ set TBB_DLL_PATH=%CONDA_PREFIX%\Library\bin
 reg add HKLM\SOFTWARE\khronos\OpenCL\Vendors /v intelocl64.dll /t REG_DWORD /d 00000000
           
           
-# gmake program arguments
+# ninja program arguments
 $cmd_args = @"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 mkdir build && cd build
 cmake -G "Ninja" -DCMAKE_BUILD_TYPE=${{ matrix.build_type }} -DCMAKE_CXX_STANDARD=${{ matrix.std }} -DCMAKE_CXX_COMPILER=${{ matrix.cxx_compiler }} -DONEDPL_BACKEND=${{ matrix.backend }} -DONEDPL_DEVICE_TYPE=${{ matrix.device_type }} ..
 ninja -j 2 -v %ninja_targets%
