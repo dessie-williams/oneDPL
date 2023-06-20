a49bb1cf8285d2fdd4417242f03b022b37d360fb
Dessie Williams <dessie.williams@intel.com>
2023-06-20 09:31:15 -0400

    Update win_test.ps1

--- .github/scripts/win_test.ps1
+++ .github/scripts/win_test.ps1
@@ -37,9 +37,9 @@ set PATH=%CONDA_PREFIX%\Library\lib;%PATH%
 set CPATH=%CONDA_PREFIX%\include
 set TBB_DLL_PATH=%CONDA_PREFIX%\Library\bin
 reg add HKLM\SOFTWARE\khronos\OpenCL\Vendors /v intelocl64.dll /t REG_DWORD /d 00000000
-cmake -G "Ninja" -DCMAKE_BUILD_TYPE=${{ matrix.build_type }} -DCMAKE_CXX_STANDARD=${{ matrix.std }} -DCMAKE_CXX_COMPILER=${{ matrix.cxx_compiler }} -DONEDPL_BACKEND=${{ matrix.backend }} -DONEDPL_DEVICE_TYPE=${{ matrix.device_type }} ..
+cmake -G "Ninja" -DCMAKE_BUILD_TYPE=(${env:matrix.build_type} -DCMAKE_CXX_STANDARD=(${env:matrix.std} -DCMAKE_CXX_COMPILER=(${env:matrix.cxx_compiler} -DONEDPL_BACKEND=(${env:matrix.backend} -DONEDPL_DEVICE_TYPE=(${env:matrix.device_type} ..
 ninja -j 2 -v %ninja_targets%
-ctest --timeout %TEST_TIMEOUT% -C ${{ matrix.build_type }} --output-on-failure %ctest_flags%        
+ctest --timeout %TEST_TIMEOUT% -C (${env:matrix.build_type} --output-on-failure %ctest_flags%        
 "@
 
 $cmd_args = [string]::join(" ", ($cmd_args.Split("`n")))
